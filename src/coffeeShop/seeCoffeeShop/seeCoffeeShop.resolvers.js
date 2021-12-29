f9e82dd3ce76dc85eae11d32fd56c26e178ce926
hi6724 <hi6724@gmail.com>
2021-12-29 16:03:42 +0900

    3 page

--- src/coffeeShop/seeCoffeeShop/seeCoffeeShop.resolvers.js
+++ src/coffeeShop/seeCoffeeShop/seeCoffeeShop.resolvers.js
@@ -7,15 +7,15 @@ export default {
     seeCoffeeShops: async (_, { page }) => {
       const totalPages = await client.coffeeShop.count();
       const coffeeShops = client.coffeeShop.findMany({
-        skip: (page - 1) * 9,
-        take: 9,
+        skip: (page - 1) * 3,
+        take: 3,
         orderBy: {
           id: "desc",
         },
       });
       return {
         CoffeeShops: coffeeShops,
-        totalPages: Math.ceil(totalPages / 9),
+        totalPages: Math.ceil(totalPages / 3),
       };
     },
   },
